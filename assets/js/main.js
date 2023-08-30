fa987763e23a087a93ea6bbc3a5f4f9bea990684
Vinay Danidhariya <vinaydanidhariya4114@gmail.com>
2023-08-30 20:13:02 +0530

    "hover button home baneer"

--- assets/js/main.js
+++ assets/js/main.js
@@ -422,9 +422,9 @@ jQuery(
 		});
 
 		// Switch Btn
-		$("body").append(
-			"<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>"
-		);
+		// $("body").append(
+		// 	"<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>"
+		// );
 	})(jQuery)
 );
 
